3e2d78d9e13ceac9a0a5b4d47be8cb0218107aea
georgejeffery <georgejeffery@gmail.com>
2019-04-25 12:28:28 +0100

    Added Join Table

---
+++ mgp-backend/app/views/person_tags/_person_tag.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! person_tag, :id, :tag_id, :person_id, :created_at, :updated_at
+json.url person_tag_url(person_tag, format: :json)
---
+++ mgp-backend/db/migrate/20190425112211_create_person_tags.rb
@@ -0,0 +1,10 @@
+class CreatePersonTags < ActiveRecord::Migration[5.2]
+  def change
+    create_table :person_tags, id:false do |t|
+      t.belongs_to :tag, index:true
+      t.belongs_to :person, index:true
+
+      t.timestamps
+    end
+  end
+end
